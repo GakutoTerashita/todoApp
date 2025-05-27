13c5a061c913e4a1179a1556d143db5f297db780
Gakuto Terashita <gakuto.terashita@jp.ricoh.com>
2025-05-27 17:17:13 +0900

    items/delete/:id エンドポイントを仮実装

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,15 @@ class TodoListItem {
     }
 }
 
+// 仮置きのデータ あとでデータベースに置き換える
+let items: TodoListItem[] = [
+    new TodoListItem('Buy groceries', 'Milk, Bread, Eggs', '1'),
+    new TodoListItem('Walk the dog', 'Take the dog for a walk in the park', '2'),
+    new TodoListItem('Read a book', 'Finish reading "The Great Gatsby"', '3'),
+    new TodoListItem('Prepare dinner', 'Cook pasta and salad for dinner', '4'),
+    new TodoListItem('Call mom', 'Check in with mom and see how she is doing', '5'),   
+];
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -24,18 +33,16 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    const items: TodoListItem[] = [
-        new TodoListItem('Buy groceries', 'Milk, Bread, Eggs', '1'),
-        new TodoListItem('Walk the dog', 'Take the dog for a walk in the park', '2'),
-        new TodoListItem('Read a book', 'Finish reading "The Great Gatsby"', '3'),
-        new TodoListItem('Prepare dinner', 'Cook pasta and salad for dinner', '4'),
-        new TodoListItem('Call mom', 'Check in with mom and see how she is doing', '5'),   
-    ]
-
     res.render('home', { 
         title: 'Home',
         items,
     });
+});
+
+app.post('/items/delete/:id', (req, res) => {
+    const itemId = req.params.id;
+    items = items.filter(item => item.id !== itemId);
+    res.redirect('/'); 
 });
 
 app.listen(PORT, () => {
