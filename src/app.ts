cd98debe152eb123f4b3076202b6fba85f8f0fca
Gakuto Terashita <gakuto.terashita@jp.ricoh.com>
2025-05-27 16:40:00 +0900

    基礎

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,18 @@
 import express from 'express';
 import path from 'path';
 
+class TodoListItem {
+    name: string;
+    description: string;
+    id: string;
+
+    constructor(name: string, description: string, id: string) {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+    }
+}
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -12,9 +24,17 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
+    const items: TodoListItem[] = [
+        new TodoListItem('Buy groceries', 'Milk, Bread, Eggs', '1'),
+        new TodoListItem('Walk the dog', 'Take the dog for a walk in the park', '2'),
+        new TodoListItem('Read a book', 'Finish reading "The Great Gatsby"', '3'),
+        new TodoListItem('Prepare dinner', 'Cook pasta and salad for dinner', '4'),
+        new TodoListItem('Call mom', 'Check in with mom and see how she is doing', '5'),   
+    ]
+
     res.render('home', { 
         title: 'Home',
-        body: 'Welcome to My App',
+        items,
     });
 });
 
