934f7896c2d562755edb356d7b0d378a578255f1
Gakuto Terashita <gakuto.terashita@jp.ricoh.com>
2025-05-29 10:05:56 +0900

    多分この方がみやすい

--- src/app.ts
+++ src/app.ts
@@ -11,28 +11,28 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../public')));
-app.use(session({
-    secret: process.env.SESSION_SECRET || 'defaultShouldNotBeUsedInProduction',
-    saveUninitialized: true,
-    resave: false,
-    cookie: {
-        httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 24, // 1 day
-    }
-}));
-app.use(flash());
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
 async function main() {
+    const PORT = process.env.PORT || 3000;
     const dbConnection = await connectDb();
+
     await createTables(dbConnection);
+
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.static(path.join(__dirname, '../public')));
+    app.use(session({
+        secret: process.env.SESSION_SECRET || 'defaultShouldNotBeUsedInProduction',
+        saveUninitialized: true,
+        resave: false,
+        cookie: {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24, // 1 day
+        },
+    }));
+    app.use(flash());
+    app.set('view engine', 'ejs');
+    app.set('views', path.join(__dirname, 'views'));
 
     app.use('/', todoRoutes(dbConnection));
 
@@ -45,4 +45,5 @@ main().catch((error) => {
     console.error('Error in main function:', error);
     process.exit(1);
 });
+
 export default app;
