da2c23b28c2d3858eb0674bc04c52357bd23cd89
Gakuto Terashita <gakuto.terashita@jp.ricoh.com>
2025-06-05 16:20:06 +0900

    だから、 Promise を返す関数が async というわけではないんだ

--- src/auth/utils.ts
+++ src/auth/utils.ts
@@ -62,7 +62,7 @@ export class AuthenticateUtil {
     };
 
     // Helper method to promisify bcrypt.compare
-    private async _comparePasswordAsync(password: string, hash: string): Promise<boolean> {
+    private _comparePasswordAsync(password: string, hash: string): Promise<boolean> {
         return new Promise((resolve, reject) => {
             bcrypt.compare(password, hash, (err, result) => {
                 if (err) reject(err);
