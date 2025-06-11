900166613fc91cd8fb1a4a828cf927018412664b
Gakuto Terashita <gakuto.terashita@jp.ricoh.com>
2025-06-11 13:47:00 +0900

    これでログがきれいになる

--- src/routes/todoRoutes.test.ts
+++ src/routes/todoRoutes.test.ts
@@ -3,8 +3,17 @@ import { handler_get_root } from './handlers/todoRouteHandlers';
 describe('todoRouteHandlers', () => {
     describe('handler_get_root', () => {
         describe('failure cases', () => {
+            let consoleErrorSpy: jest.SpyInstance;
+            let consoleLogSpy: jest.SpyInstance;
+            beforeEach(() => {
+                consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => { });
+                consoleLogSpy = jest.spyOn(console, 'log').mockImplementation(() => { });
+            })
+
             afterEach(() => {
                 jest.restoreAllMocks();
+                consoleErrorSpy.mockRestore();
+                consoleLogSpy.mockRestore();
             })
 
             it('should redirect to /error when finding done todo_items fails', async () => {
@@ -20,7 +29,7 @@ describe('todoRouteHandlers', () => {
                 jest.spyOn(
                     require('../services/todo-items.service'),
                     'findTodoItemsDone'
-                ).mockRejectedValue(new Error('Database error'));
+                ).mockRejectedValue(new Error('Database error 0'));
 
                 await handler_get_root(req, res);
 
@@ -46,7 +55,7 @@ describe('todoRouteHandlers', () => {
                     require('../services/todo-items.service'),
                     'findTodoItemsNotDone'
                 )
-                    .mockRejectedValue(new Error('Database error'));
+                    .mockRejectedValue(new Error('Database error 1'));
 
                 await handler_get_root(req, res);
 
