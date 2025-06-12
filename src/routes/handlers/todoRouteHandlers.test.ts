3a00850d70587ccc3b4d1e7566c06111114e1b16
Gakuto Terashita <gakuto.terashita@jp.ricoh.com>
2025-06-12 14:41:50 +0900

    テスト追加

--- src/routes/handlers/todoRouteHandlers.test.ts
+++ src/routes/handlers/todoRouteHandlers.test.ts
@@ -1,4 +1,4 @@
-import { get_todo_root } from './todoRouteHandlers';
+import { get_todo_root, post_delete_itemid } from './todoRouteHandlers';
 import * as todoItemService from '../../services/todo-items.service';
 import { TodoListItem } from '../../db/todoListItem';
 
@@ -121,6 +121,87 @@ describe('todoRouteHandlers', () => {
                     success: req.flash('success'),
                     error: req.flash('error'),
                 });
+            });
+        });
+    });
+
+    describe('post_delete_itemid', () => {
+        describe('failure cases', () => {
+            let consoleErrorSpy: jest.SpyInstance;
+            beforeEach(() => {
+                consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => { });
+            });
+
+            afterEach(() => {
+                jest.restoreAllMocks();
+                consoleErrorSpy.mockRestore();
+            });
+
+            it('should redirect with error if itemId is not provided', async () => {
+                const req = {
+                    params: {},
+                    flash: jest.fn(),
+                } as any;
+                const res = {
+                    redirect: jest.fn(),
+                } as any;
+
+                await post_delete_itemid(req, res);
+
+                expect(req.flash).toHaveBeenCalledWith('error', 'Item ID is required');
+                expect(res.redirect).toHaveBeenCalledWith('/');
+            });
+
+            it('should redirect with error if removing item fails', async () => {
+                const req = {
+                    params: { itemId: '1' },
+                    flash: jest.fn(),
+                } as any;
+                const res = {
+                    redirect: jest.fn(),
+                } as any;
+
+                jest.spyOn(
+                    todoItemService,
+                    'removeTodoItem'
+                ).mockRejectedValue(new Error('Database error 2'));
+
+                await post_delete_itemid(req, res);
+
+                expect(req.flash).toHaveBeenCalledWith('error', 'Failed to remove item');
+                expect(res.redirect).toHaveBeenCalledWith('/');
+            });
+        });
+
+        describe('success cases', () => {
+            let consoleErrorSpy: jest.SpyInstance;
+            beforeEach(() => {
+                consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => { });
+            });
+
+            afterEach(() => {
+                jest.restoreAllMocks();
+                consoleErrorSpy.mockRestore();
+            });
+
+            it('redirect with success', async () => {
+                const req = {
+                    params: { itemId: '1' },
+                    flash: jest.fn(),
+                } as any;
+                const res = {
+                    redirect: jest.fn(),
+                } as any;
+
+                jest.spyOn(
+                    todoItemService,
+                    'removeTodoItem'
+                ).mockResolvedValue();
+
+                await post_delete_itemid(req, res);
+
+                expect(req.flash).toHaveBeenCalledWith('success', 'Removed item successfully');
+                expect(res.redirect).toHaveBeenCalledWith('/');
             });
         });
     });
